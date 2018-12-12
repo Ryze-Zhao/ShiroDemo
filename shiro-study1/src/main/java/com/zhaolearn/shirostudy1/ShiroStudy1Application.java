ddaf42d49e0808ab5994b7cba38f7e770e979302
RyzeZhao <643080112@qq.com>
2018-12-12 16:07:16 +0800

    跟着视频敲代码

---
+++ shiro-study1/src/main/java/com/zhaolearn/shirostudy1/ShiroStudy1Application.java
@@ -0,0 +1,12 @@
+package com.zhaolearn.shirostudy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroStudy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroStudy1Application.class, args);
+	}
+}
---
+++ shiro-study1/src/main/java/com/zhaolearn/shirostudy1/shiro/ShiroConfig.java
@@ -0,0 +1,31 @@
+package com.zhaolearn.shirostudy1.shiro;
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+    //创建ShiroFilterFactoryBean
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("defaultWebSecurityManager") DefaultWebSecurityManager defaultWebSecurityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(defaultWebSecurityManager);
+        return shiroFilterFactoryBean;
+    }
+
+    //创建DefaultWebSecurityManager
+    @Bean("defaultWebSecurityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm") UserRealm userRealm) {
+        DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
+        defaultWebSecurityManager.setRealm(userRealm);
+        return defaultWebSecurityManager;
+    }
+
+    //创建Realm,返回的值放入Spring
+    @Bean("userRealm")
+    public UserRealm getRealm() {
+        return new UserRealm();
+    }
+}
