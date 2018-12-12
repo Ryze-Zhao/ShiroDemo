106e4a9ae3f8e89904f12879ea3de21f503ffc93
RyzeZhao <643080112@qq.com>
2018-12-12 14:39:43 +0800

    简单使用

---
+++ shiro-demo1/src/main/java/com/zhaolearn/shirodemo1/ShiroDemo1Application.java
@@ -0,0 +1,12 @@
+package com.zhaolearn.shirodemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroDemo1Application.class, args);
+    }
+}
