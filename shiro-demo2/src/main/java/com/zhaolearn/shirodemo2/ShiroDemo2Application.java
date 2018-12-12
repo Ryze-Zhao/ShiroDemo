33839e0bfeac85472182c13b4644419481be6b80
RyzeZhao <643080112@qq.com>
2018-12-12 15:15:57 +0800

    mysql

---
+++ shiro-demo2/src/main/java/com/zhaolearn/shirodemo2/ShiroDemo2Application.java
@@ -0,0 +1,12 @@
+package com.zhaolearn.shirodemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroDemo2Application.class, args);
+    }
+}
