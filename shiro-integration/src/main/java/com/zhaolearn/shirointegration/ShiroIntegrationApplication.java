61c5fd53dbf1f09617748fcef8244343169998d1
RyzeZhao <643080112@qq.com>
2018-12-13 11:17:14 +0800

    SpringBoot2.1.1+Shiro1.4.0+JPA+Thymeleaf整合演示初始化

---
+++ shiro-integration/src/main/java/com/zhaolearn/shirointegration/ShiroIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.zhaolearn.shirointegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroIntegrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroIntegrationApplication.class, args);
+    }
+
+}
+
