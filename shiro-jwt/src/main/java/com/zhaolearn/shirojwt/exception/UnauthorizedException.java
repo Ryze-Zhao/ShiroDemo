9914ef9594d304780a2d332b64fb12d7aa0a0b55
RyzeZhao <643080112@qq.com>
2018-12-17 16:32:12 +0800

    jwt初始化，使用别人的https://github.com/Smith-Cruise/Spring-Boot-Shiro，能用的

---
+++ shiro-jwt/src/main/java/com/zhaolearn/shirojwt/exception/UnauthorizedException.java
@@ -0,0 +1,11 @@
+package com.zhaolearn.shirojwt.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String msg) {
+        super(msg);
+    }
+
+    public UnauthorizedException() {
+        super();
+    }
+}
--- shiro-jwt/src/main/java/com/zhaolearn/shirojwt/shiro/ShiroConfig.java
+++ shiro-jwt/src/main/java/com/zhaolearn/shirojwt/shiro/ShiroConfig.java
@@ -1,96 +1,69 @@
 package com.zhaolearn.shirojwt.shiro;
 
-import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
+import org.apache.shiro.mgt.DefaultSessionStorageEvaluator;
+import org.apache.shiro.mgt.DefaultSubjectDAO;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.web.filter.DelegatingFilterProxy;
 
 import javax.servlet.Filter;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class ShiroConfig {
-    private final static Logger logger = LoggerFactory.getLogger(ShiroConfig.class);
 
-    @Bean("filterRegistrationBean")
-    public FilterRegistrationBean delegatingFilterProxy(){
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-        DelegatingFilterProxy proxy = new DelegatingFilterProxy();
-        proxy.setTargetFilterLifecycle(true);
-        proxy.setTargetBeanName("shiroFilterFactoryBean");
-        filterRegistrationBean.setFilter(proxy);
-        return filterRegistrationBean;
-    }
+    @Bean("securityManager")
+    public DefaultWebSecurityManager getManager(MyRealm realm) {
+        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+        // 使用自己的realm
+        manager.setRealm(realm);
 
-    //创建ShiroFilterFactoryBean
-    @Bean("shiroFilterFactoryBean")
-    public ShiroFilterFactoryBean getShiroFilterFactoryBean(
-            @Qualifier("defaultWebSecurityManager") DefaultWebSecurityManager defaultWebSecurityManager) {
-        logger.info("jinlaile");
-        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-        shiroFilterFactoryBean.setSecurityManager(defaultWebSecurityManager);
         /*
-         * 4    Shiro内置过滤器，可以实现权限相关的拦截器，常用过滤器
-         * 4.1    anon：无需认证
-         * 4.2    authc：必须认证
-         * 4.3    user：如果使用rememberMe功能可以直接访问
-         * 4.4    perms[]：该资源必须有该权限才可以访问
-         * 4.5    roles[]：该资源必须有该角色才可以访问
+         * 关闭shiro自带的session，详情见文档
+         * http://shiro.apache.org/session-management.html#SessionManagement-StatelessApplications%28Sessionless%29
          */
-        Map<String, String> filterChainMap = new LinkedHashMap<>(); //为了保证有序采用Linked，key是拦截路径，value是过滤器
-        filterChainMap.put("/demo/index", "anon");
-        filterChainMap.put("/demo/tologin", "anon");
-        filterChainMap.put("/demo/login", "anon");
-        //      filterChainMap.put("/demo/add", "myRoleFilter[perm1,perm3]");
-        //    filterChainMap.put("/demo/update", "myRoleFilter[admin,test]");
-        //一个目录下可以使用这个，这个是controller路径
-        filterChainMap.put("/demo/*", "authc");
-        shiroFilterFactoryBean.setLoginUrl("/demo/tologin");//拦截后跳转到的页面
-        shiroFilterFactoryBean.setUnauthorizedUrl("/demo/index");//未授权自动跳转到的页面
-        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainMap);
-        return shiroFilterFactoryBean;
+        DefaultSubjectDAO subjectDAO = new DefaultSubjectDAO();
+        DefaultSessionStorageEvaluator defaultSessionStorageEvaluator = new DefaultSessionStorageEvaluator();
+        defaultSessionStorageEvaluator.setSessionStorageEnabled(false);
+        subjectDAO.setSessionStorageEvaluator(defaultSessionStorageEvaluator);
+        manager.setSubjectDAO(subjectDAO);
+
+        return manager;
     }
 
-    //创建DefaultWebSecurityManager
-    @Bean("defaultWebSecurityManager")
-    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm") UserRealm userRealm) {
-        DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
-        defaultWebSecurityManager.setRealm(userRealm);
-        return defaultWebSecurityManager;
-    }
+    @Bean("shiroFilter")
+    public ShiroFilterFactoryBean factory(DefaultWebSecurityManager securityManager) {
+        ShiroFilterFactoryBean factoryBean = new ShiroFilterFactoryBean();
 
-    //创建Realm,返回的值放入Spring
-    @Bean("userRealm")
-    public UserRealm getRealm() {
-        return new UserRealm();
-    }
+        // 添加自己的过滤器并且取名为jwt
+        Map<String, Filter> filterMap = new HashMap<>();
+        filterMap.put("jwt", new JWTFilter());
+        factoryBean.setFilters(filterMap);
 
+        factoryBean.setSecurityManager(securityManager);
+        factoryBean.setUnauthorizedUrl("/401");
+
+        /*
+         * 自定义url规则
+         * http://shiro.apache.org/web.html#urls-
+         */
+        Map<String, String> filterRuleMap = new HashMap<>();
+        // 所有请求通过我们自己的JWT Filter
+        filterRuleMap.put("/**", "jwt");
+        // 访问401和404页面不通过我们的Filter
+        filterRuleMap.put("/401", "anon");
+        factoryBean.setFilterChainDefinitionMap(filterRuleMap);
+        return factoryBean;
+    }
 
     /**
-     * 配置ShiroDialect用于thymeleaf和shiro标签配合使用
-     */
-    @Bean("shiroDialect")
-    public ShiroDialect getShiroDialect() {
-        return new ShiroDialect();
-    }
-
-
-    /**
-     * 以下3个是开启注解
-     *
-     * @return
+     * 下面的代码是添加注解支持
      */
     @Bean
     @DependsOn("lifecycleBeanPostProcessor")
@@ -113,21 +86,4 @@ public class ShiroConfig {
         advisor.setSecurityManager(securityManager);
         return advisor;
     }
-
-    /**
-     * 注入加密方式一：
-     * * 凭证匹配器
-     * （由于我们的密码校验交给Shiro的SimpleAuthenticationInfo进行处理了 ）
-     * @return
-     */
-   /* @Bean("hashedCredentialsMatcher")
-    public HashedCredentialsMatcher hashedCredentialsMatcher(){
-        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
-        hashedCredentialsMatcher.setHashAlgorithmName("md5");//散列算法:这里使用MD5算法;
-        //   hashedCredentialsMatcher.setHashIterations(2);//散列的次数，比如散列两次，相当于 md5(md5(""));
-        hashedCredentialsMatcher.setHashIterations(1024);
-        return hashedCredentialsMatcher;
-    }*/
-
-
 }
