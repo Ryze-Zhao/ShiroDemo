49b2212d7412fe8a150ef19827a22663d3be39af
RyzeZhao <643080112@qq.com>
2018-12-18 20:51:15 +0800

    token

--- shiro-jwt/src/main/java/com/zhaolearn/shirojwt/shiro/JwtToken.java
+++ shiro-jwt/src/main/java/com/zhaolearn/shirojwt/shiro/JwtToken.java
@@ -2,11 +2,11 @@ package com.zhaolearn.shirojwt.shiro;
 
 import org.apache.shiro.authc.AuthenticationToken;
 
-public class JWTToken implements AuthenticationToken {
+public class JwtToken implements AuthenticationToken {
     // 密钥
     private String token;
 
-    public JWTToken(String token) {
+    public JwtToken(String token) {
         this.token = token;
     }
 
