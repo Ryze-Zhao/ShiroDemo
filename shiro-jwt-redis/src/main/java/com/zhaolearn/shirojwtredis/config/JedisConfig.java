671011f2e8f5eb18fbc0daf8a1771d7337532079
RyzeZhao <643080112@qq.com>
2018-12-24 10:42:09 +0800

    redis-config独立出来

--- shiro-jwt-redis/src/main/java/com/zhaolearn/shirojwtredis/config/JedisConfig.java
+++ shiro-jwt-redis/src/main/java/com/zhaolearn/shirojwtredis/config/JedisConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import redis.clients.jedis.JedisPool;
@@ -14,7 +13,7 @@ import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 @EnableAutoConfiguration
-@PropertySource("classpath:application-dev.yaml")
+@PropertySource("classpath:config/redis-config.yaml")
 @ConfigurationProperties(prefix = "redis")
 public class JedisConfig {
 
